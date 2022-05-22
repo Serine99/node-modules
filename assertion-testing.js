2c9f0da53c081c8e133f78702430c370dd57354f
Serine99 <serine.sardaryan1@gmail.com>
2022-05-22 22:06:26 +0400

    added examples of assert

--- assertion-testing.js
+++ assertion-testing.js
@@ -1,8 +1,41 @@
-import assert from 'assert/strict';
+const assert = require ('assert/strict')
 
-await assert.doesNotReject(
-  async () => {
-    throw new TypeError('Wrong value');
-  },
-  SyntaxError
-);
+// await assert.doesNotReject(
+//   async () => {
+//     throw new TypeError('Wrong value');
+//   },
+//   SyntaxError
+// );
+
+// console.log(assert);
+// const deepEqual =  assert.deepEqual([[[1, 2, 3]], 4, 5], [[[1, 2, '3']], 4, 5]);
+// console.log(deepEqual);
+// const deepEqual1 = assert.deepEqual(/a/gi, new Date());
+// console.log(deepEqual1);
+
+/* Class: assert.AssertionError ---> Indicates the failure of an assertion. All errors thrown by the assert module will be instances of the AssertionError class.*/
+
+// import assert from 'assert';
+
+// Generate an AssertionError to compare the error message later:
+const { message } = new assert.AssertionError({
+  actual: 1,
+  expected: 2,
+  operator: 'strictEqual'
+});
+
+// Verify error output:
+try {
+  assert.strictEqual(1, 2);
+} catch (err) {
+  assert(err instanceof assert.AssertionError);
+  assert.strictEqual(err.message, message);
+  assert.strictEqual(err.name, 'AssertionError');
+  assert.strictEqual(err.actual, 1);
+  assert.strictEqual(err.expected, 2);
+  assert.strictEqual(err.code, 'ERR_ASSERTION');
+  assert.strictEqual(err.operator, 'strictEqual');
+  assert.strictEqual(err.generatedMessage, true);
+}
+
+console.log({ message });
